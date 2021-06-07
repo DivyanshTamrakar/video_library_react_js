69abaa8fe1c92db6171f51cbaa1bd3e3bde15454
DivyanshTamrakar <tamrakardivyansh189@gmail.com>
2021-06-08 02:22:00 +0530

    feat : create playlist option added in modal

--- FrontEnd/src/Context/PlaylistContext.js
+++ FrontEnd/src/Context/PlaylistContext.js
@@ -6,11 +6,14 @@ export const  PlaylistContext = createContext();
 
 
 
+
 export function PlaylistProvider({children}){
+  
+  const [playlistArray,setlist] = useState([]);
   const [modal,setmodal] = useState(false);
 
    return(
-    <PlaylistContext.Provider value ={{modal,setmodal}}>
+    <PlaylistContext.Provider value ={{modal,setmodal,playlistArray,setlist}}>
     {children}
    </PlaylistContext.Provider>
  );
