7c8a1b6f7586322a1a0fc3cd15befb1e1aa2ec29
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2021-10-18 03:21:53 +0530

    style : responsive drawer

--- FrontEnd/src/Utils/fetchApi.js
+++ FrontEnd/src/Utils/fetchApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 // userId which comes from local storage
+// export const url = "https://fathomless-temple-28282.herokuapp.com";
 export const url = "http://localhost:5000";
 export async function getData(...endpoints) {
   let final_url = `${url}${endpoints}`;
